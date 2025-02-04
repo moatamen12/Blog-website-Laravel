199872c235d903f98723571f38f68594471f05e7
moatamen12 <maotamennaief@gmail.com>
2025-02-04 23:58:32 +0100

    View done

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-// use App\Http\Controllers\TestController; //including the TestController file
+use App\Http\Controllers\IndexController; //including the TestController file
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/index',function (){
-    return view('index');
-});
-// Route::get('/test',[TestController::class, 'firstAction']);
 
+Route::get('/index',[IndexController::class, 'testAction']);
+
+
+
+// for learning 
+// Route::get('/test',[TestController::class, 'firstAction']);
 // Route::get('/greet',[TestController::class, 'greet']);

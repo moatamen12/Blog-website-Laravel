f50ac0e2a9e2104b0faa647b5c3d1fcae0b7ef2c
moatamen12 <maotamennaief@gmail.com>
2025-02-04 23:25:25 +0100

    start of app

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
-class TestController extends Controller
-{
-    public function firstAction(){ //Action Methode 
-        $localname = 'moatamen';
-        $newBooks = ['html','js','css'];
-        return view('/test',['name' => $localname, 'books' => $newBooks]);
-    }
-    public function greet(){
-        return 'Hello this is greet action ';
-    }
-}
+// class TestController extends Controller
+// {
+//     public function firstAction(){ //Action Methode 
+//         $localname = 'moatamen';
+//         $newBooks = ['html','js','css'];
+//         return view('/test',['name' => $localname, 'books' => $newBooks]);
+//     }
+//     public function greet(){
+//         return 'Hello this is greet action ';
+//     }
+// }
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TestController; //including the TestController file
+// use App\Http\Controllers\TestController; //including the TestController file
 
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/index',function (){
+    return view('index');
+});
 // Route::get('/test',[TestController::class, 'firstAction']);
 
 // Route::get('/greet',[TestController::class, 'greet']);
